39c70eea1a4a381f46c408218558b57ec14c7b44
Deepak <d.kadarivel@relay42.com>
2019-03-01 13:38:10 +0100

    Updates

---
+++ precache-manifest.100e66f308aa257b4b1b5450a2674819.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bill-scanner/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "852eef8b227ed3456578",
+    "url": "/bill-scanner/static/js/runtime~main.852eef8b.js"
+  },
+  {
+    "revision": "cce39fd705623fcd205d",
+    "url": "/bill-scanner/static/js/main.cce39fd7.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/bill-scanner/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "cce39fd705623fcd205d",
+    "url": "/bill-scanner/static/css/main.60403ca3.chunk.css"
+  },
+  {
+    "revision": "ce76c4045b2725dad59b00f3896de9d5",
+    "url": "/bill-scanner/index.html"
+  }
+];
